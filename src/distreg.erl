0b3855508acfe3e6250fc4262c81a6ef1edc8792
Paul Schoenfelder <paulschoenfelder@gmail.com>
2016-06-22 02:10:05 -0500

    Export reg_global/2

--- src/distreg.erl
+++ src/distreg.erl
@@ -4,6 +4,7 @@
 -export([reg/1,reg/2,unreg/1,track/1, whereis/1,
 				 call/2,cast/2,inform/2,start/2,
 				 procinfo/1,node_for_name/1,processes/0,
+                 reg_global/2,
 				 % Used internally do not call from client.
 				 node_for_hash/2]).
 -include("distreg.hrl").
